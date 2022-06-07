db1cfe1168da102da7c300e3ab3405f2ed756ec1
NotQuiteAmish <calebhostetler@yahoo.com>
2022-06-07 09:35:04 -0400

    Add initial files

---
+++ zq-forcing-alg.py
@@ -0,0 +1,8 @@
+# Algorithm to compute Z_q(G) for an arbitrary graph
+
+# Algorithm comes from "Using Variants of Zero Forcing to Bound the Inertia Set of a Graph" by S. Butler, J. Grout, and H. Tracy Hall
+
+import networkx as nx
+
+def calculate_zq(graph, q):
+    pass
