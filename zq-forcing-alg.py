f53289d8bdf23f169d163641c607c98b538991ff
danielogbe <64997148+danielogbe@users.noreply.github.com>
2022-06-21 17:00:11 -0400

    Refactored the code to a new file

--- zq-forcing-alg.py
+++ zq-forcing-alg.py
@@ -97,14 +97,3 @@ matplotlib.pyplot.show()
 # print(calculate_zq(graph, 2))
 
 
-import math
-def halfing_number(n):
-    """
-    halfing_number refers to the number of tokens spent in a caterpillar graph when n=2 and the ends
-    are colored to color all the center vertices
-    """
-    if n<=2:
-        return 0
-    #we remove one vertex(center) and take the lower half. 
-    next = n - 1 - math.floor((n-1)/2)
-    return 1 + halfing_number(next)
